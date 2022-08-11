c4c61fb6db1cda4f64320d5e6adc5ff7155ce4f4
Fabio Gomes <fabio.sgomes@dataprev.gov.br>
2022-08-11 07:28:54 -0300

    iniciando os trabalhos com Array

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -2,7 +2,23 @@ package br.com.alura.comex;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-  }
+	public static void main(String[] args) {
+		Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+
+		mostraRelatorioComNomeDosClientesComArray(pedidos);
+	}
+
+	private static void mostraRelatorioComNomeDosClientesComArray(Pedido[] pedidos) {
+		System.out.println("=================");
+		System.out.println("Nome dos Clientes");
+		System.out.println("Estrutura: Array");
+		System.out.println("=================");
+
+		for (Pedido pedido : pedidos) {
+			if(pedido != null)
+				System.out.println(pedido.getCliente());
+		}
+
+		System.out.println("=================");
+	}
 }
