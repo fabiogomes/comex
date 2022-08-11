1e320910c62f04eb5910d16f3371285cb0a645c1
Fabio Gomes <fabio.sgomes@dataprev.gov.br>
2022-08-11 07:50:42 -0300

    Usar List ao invés do Array

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,11 +1,34 @@
 package br.com.alura.comex;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
+		// Utilizando Array
+		/*
 		Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
 
 		mostraRelatorioComNomeDosClientesComArray(pedidos);
+		*/
+
+		// Utilizando List
+		List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+
+		mostraRelatorioComNomeDosClientesComList(pedidos);
+	}
+
+	private static void mostraRelatorioComNomeDosClientesComList(List<Pedido> pedidos) {
+		System.out.println("=================");
+		System.out.println("Nome dos Clientes");
+		System.out.println("Estrutura: List");
+		System.out.println("=================");
+
+		for (Pedido pedido : pedidos) {
+				System.out.println(pedido.getCliente());
+		}
+
+		System.out.println("=================");
 	}
 
 	private static void mostraRelatorioComNomeDosClientesComArray(Pedido[] pedidos) {
--- src/br/com/alura/comex/ProcessadorDeCsv.java
+++ src/br/com/alura/comex/ProcessadorDeCsv.java
@@ -7,48 +7,51 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProcessadorDeCsv {
 
-  public static Pedido[] processaArquivo(String nomeDoArquivo) {
-    try {
-      URL recursoCSV = ClassLoader.getSystemResource(nomeDoArquivo);
-      Path caminhoDoArquivo = Path.of(recursoCSV.toURI());
+	public static List<Pedido> processaArquivo(String nomeDoArquivo) {
+		try {
+			URL recursoCSV = ClassLoader.getSystemResource(nomeDoArquivo);
+			Path caminhoDoArquivo = Path.of(recursoCSV.toURI());
 
-      Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
+			Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
 
-      leitorDeLinhas.nextLine();
+			leitorDeLinhas.nextLine();
 
-      Pedido[] pedidos = new Pedido[10];
+			// Pedido[] pedidos = new Pedido[10];
+			List<Pedido> pedidos = new ArrayList<>();
 
-      int quantidadeDeRegistros = 0;
-      while (leitorDeLinhas.hasNextLine()) {
-        String linha = leitorDeLinhas.nextLine();
-        String[] registro = linha.split(",");
+			int quantidadeDeRegistros = 0;
+			while (leitorDeLinhas.hasNextLine()) {
+				String linha = leitorDeLinhas.nextLine();
+				String[] registro = linha.split(",");
 
-        String categoria = registro[0];
-        String produto = registro[1];
-        BigDecimal preco = new BigDecimal(registro[2]);
-        int quantidade = Integer.parseInt(registro[3]);
-        LocalDate data = LocalDate.parse(registro[4], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        String cliente = registro[5];
+				String categoria = registro[0];
+				String produto = registro[1];
+				BigDecimal preco = new BigDecimal(registro[2]);
+				int quantidade = Integer.parseInt(registro[3]);
+				LocalDate data = LocalDate.parse(registro[4], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				String cliente = registro[5];
 
-        Pedido pedido = new Pedido(categoria, produto, cliente, preco, quantidade, data);
-        pedidos[quantidadeDeRegistros] = pedido;
+				Pedido pedido = new Pedido(categoria, produto, cliente, preco, quantidade, data);
+				pedidos.add(pedido);
+				/*
+				 * pedidos[quantidadeDeRegistros] = pedido;
+				 * 
+				 * quantidadeDeRegistros++; if (pedidos[pedidos.length - 1] != null) { pedidos =
+				 * Arrays.copyOf(pedidos, pedidos.length * 2); }
+				 */
+			}
 
-        quantidadeDeRegistros++;
-        if (pedidos[pedidos.length - 1] != null) {
-          pedidos = Arrays.copyOf(pedidos, pedidos.length * 2);
-        }
-      }
-
-      return pedidos;
-    } catch (URISyntaxException e) {
-      throw new RuntimeException(String.format("Arquivo {} não localizado!", nomeDoArquivo));
-    } catch (IOException e) {
-      throw new RuntimeException("Erro ao abrir Scanner para processar arquivo!");
-    }
-  }
+			return pedidos;
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(String.format("Arquivo {} não localizado!", nomeDoArquivo));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao abrir Scanner para processar arquivo!");
+		}
+	}
 }
